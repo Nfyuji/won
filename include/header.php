cb9eee802e05bd471100361648c89527aa942cfb
Nfyuji <134148566+Nfyuji@users.noreply.github.com>
2025-03-09 01:30:56 +0300

    Add files via upload

---
+++ include/header.php
@@ -0,0 +1,17 @@
+<!-- header.php -->
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>موقع التسجيل</title>
+    <link rel="stylesheet" href="styles.css"> <!-- أضف رابط إلى ملفات CSS الخاصة بك -->
+</head>
+<body>
+    <header>
+        <div class="container text-center">
+            <h1>اربح مع نور</h1>
+            <p>باقي على فتح التسجيل:</p>
+            <p class="time">13 يوم 11 ساعة 41 دقيقة 51 ثانية</p>
+        </div>
+    </header>
